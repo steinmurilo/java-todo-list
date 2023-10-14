ea04cd35a2b9ad70965834e6ceee973392b8413e
Murilo Stein <contato.mrstein@gmail.com>
2023-10-14 12:06:55 -0300

    first commit

---
+++ src/main/java/br/com/mrstein/javatodolist/JavaTodoListApplication.java
@@ -0,0 +1,13 @@
+package br.com.mrstein.javatodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTodoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mrstein/javatodolist/JavaTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mrstein.javatodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
